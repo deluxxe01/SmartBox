07209b79fd4148c5d8367c41c92258f22f480394
CAIO LORRAM VALENTE <caio_valente@estudante.sesisenai.org.br>
2025-09-04 15:25:00 -0300

    erros de commit

--- SmartBox_code/Front_end/src/Routes/Rotes.jsx
+++ SmartBox_code/Front_end/src/Routes/Rotes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/LoginPage/Login";
 import Cadastro from "../Pages/CadastroPage/Cadastro";
 import CatalogoPage from "../Pages/CatalogoPage/CatalogoPage";
+<<<<<<< HEAD
 import Home from "../Pages/HomePage/Home"
 
 const router =  createBrowserRouter([
@@ -10,6 +11,17 @@ const router =  createBrowserRouter([
 {path: "/catalogo", element: <CatalogoPage />},
 {path: "/login", element: <Login />},
 {path: "/cadastro", element: <Cadastro />}
+=======
+import Carrinho from "../Pages/Carrinho/Carrinho";
+
+const router =  createBrowserRouter([
+
+{path: "/", element: <LandingPage />},
+{path: "/catalogo", element: <CatalogoPage />},
+{path:'/Carrinho',element:<Carrinho />}
+
+
+>>>>>>> 40030e1 (resolvendo o erro de conflito)
 
 ])
 
