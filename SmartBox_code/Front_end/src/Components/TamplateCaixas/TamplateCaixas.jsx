40030e1b8d382f879033faf97047a4b816d80129
CAIO LORRAM VALENTE <caio_valente@estudante.sesisenai.org.br>
2025-09-04 14:59:39 -0300

    resolvendo o erro de conflito

---
+++ SmartBox_code/Front_end/src/Components/TamplateCaixas/TamplateCaixas.jsx
@@ -0,0 +1,14 @@
+ 
+ function TamplateCaixa(){
+    return(
+        <div className=" containerTamplateCaixas">
+            <h1>Carrinho</h1>
+            <main className="containerCaixas">
+                
+
+            </main>
+
+        </div>
+    )
+ }
+ export default TamplateCaixa
--- SmartBox_code/Front_end/src/Routes/Rotes.jsx
+++ SmartBox_code/Front_end/src/Routes/Rotes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "../Pages/LandingPage/LandingPage";
 import CatalogoPage from "../Pages/CatalogoPage/CatalogoPage";
+import Carrinho from "../Pages/Carrinho/Carrinho";
 
 const router =  createBrowserRouter([
 
 {path: "/", element: <LandingPage />},
-{path: "/catalogo", element: <CatalogoPage />}
+{path: "/catalogo", element: <CatalogoPage />},
+{path:'/Carrinho',element:<Carrinho />}
 
 
 
