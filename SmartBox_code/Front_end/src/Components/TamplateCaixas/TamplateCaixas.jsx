8e0d7ffc75a9faa86a4c9b38d28d31cf1025a033
CAIO LORRAM VALENTE <caio_valente@estudante.sesisenai.org.br>
2025-09-04 17:12:36 -0300

    carrinho do client

--- SmartBox_code/Front_end/src/Components/TamplateCaixas/TamplateCaixas.jsx
+++ SmartBox_code/Front_end/src/Components/TamplateCaixas/TamplateCaixas.jsx
@@ -1,9 +1,25 @@
- 
- function TamplateCaixa(){
+  import { useState } from "react"
+  import './TamplateCaixas.css'
+ function TamplateCaixa(props){
+    const [numerosCaixas,setNumeroCaixas] = useState(0)
+    
     return(
         <div className=" containerTamplateCaixas">
             <h1>Carrinho</h1>
             <main className="containerCaixas">
+                <div className="containerCarrinhoCaixa">
+                    <img src={props.img ? props.img : 'https://i.ibb.co/Z6yfsTRY/image-2-1-5.png'} />
+                    <div> <h2 className="h2NomeDaCaixa">{props.nome ? props.nome :'Caixa esquisita'}</h2></div>
+                    <div className="containerCaixaPreçoQtd">
+                       <p className="pPrecoCaixa">R$ 10</p>
+                       <div className="containerNumeroCaixas">
+                            <p className="iconApagarCaixa">🚽</p>
+                            <button onClick={()=>{setNumeroCaixas(numerosCaixas-1)}}>-</button>
+                            {numerosCaixas}
+                            <button onClick={()=>{setNumeroCaixas(numerosCaixas+1)}}>+</button>
+                        </div>
+                    </div>
+                </div>
                 
 
             </main>
--- SmartBox_code/Front_end/src/Routes/Rotes.jsx
+++ SmartBox_code/Front_end/src/Routes/Rotes.jsx
@@ -2,36 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../Pages/LoginPage/Login";
 import Cadastro from "../Pages/CadastroPage/Cadastro";
 import CatalogoPage from "../Pages/CatalogoPage/CatalogoPage";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Home from "../Pages/HomePage/Home"
+import Carrinho from "../Pages/Carrinho/Carrinho";
 
 const router =  createBrowserRouter([
 
 {path:"/", element: <Home />},
 {path: "/catalogo", element: <CatalogoPage />},
 {path: "/login", element: <Login />},
-{path: "/cadastro", element: <Cadastro />}
-=======
-=======
->>>>>>> 40030e1b8d382f879033faf97047a4b816d80129
-import Carrinho from "../Pages/Carrinho/Carrinho";
-
-const router =  createBrowserRouter([
-
-{path: "/", element: <LandingPage />},
-{path: "/catalogo", element: <CatalogoPage />},
-{path:'/Carrinho',element:<Carrinho />}
-
-
->>>>>>> 40030e1 (resolvendo o erro de conflito)
+{path: "/cadastro", element: <Cadastro />},
+{path:"/carinho",element:<Carrinho />}
 
 ])
-
-
-
-
-
-
 
 export default router
