22f4bb57f9625e059056f586958eda8fc91b1144
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2025-09-11 17:02:51 -0300

    fazendo a page de personalizar a caixa

--- SmartBox_code/Front_end/src/Pages/Caixa_Persona_etp1/Caixa_persona_etp1.jsx
+++ SmartBox_code/Front_end/src/Pages/Caixa_Persona_etp1/Caixa_persona_etp1.jsx
@@ -1,63 +1,120 @@
-
-import './Caixa_persona_etp1.css'
-import NavBar from '../../Components/NavBar/NavBar'
+import './Caixa_persona_etp1.css';
+import NavBar from '../../Components/NavBar/NavBar';
 
 function Caixa_persona_etp1() {
   return (
     <div>
-      <nav><NavBar/></nav>
+      {/* Navbar */}
+      <nav>
+        <NavBar />
+      </nav>
 
-   <div className='conatiner_page_caixa_persona'>
+      {/* Container principal */}
+      <div className="conatiner_page_caixa_persona">
+        
+        {/* Lado Preview 3D */}
+        <div className="conatiner_m3D">
+          <img
+            src="https://i1.sndcdn.com/artworks-7n7INsM9YyE3b2KH-kyEyFw-t1080x1080.jpg"
+            alt="Preview Caixa"
+            className="no_biches_img"
+          />
+        </div>
+
+        {/* Lado Personalização */}
+        <div className="container_personalizacao_caixa">
+          
+          {/* Título */}
+          <div className="container_h1_caixa_personalizada_etp1">
+            <h1 className="h1_caixa_personalizada_etp1">
+              Personalização da caixa: 1° andar
+            </h1>
+          </div>
+
+          {/* Seleção de andares */}
+          <div className="container_infos_andares_caixa">
+            <div className="container_andares_caixa">
+              <h2>Andares</h2>
+            </div>
+            <div className="container_numero_andares">
+              <h2>1° andar</h2>
+              <h2>2° andar</h2>
+              <h2>3° andar</h2>
+            </div>
+          </div>
+
+          {/* Cor do chassi */}
+          <div className="container_cor_chasi">
+            <div className="container_cor_chasi_h1">
+              <h1>Cor do chassi</h1>
+            </div>
+            <div className="cores_chasi">
+              <button>Vermelho</button>
+              <button>Preto</button>
+              <button>Azul</button>
+            </div>
+          </div>
+
+          {/* Cor da paleta esquerda */}
+          <div className="container_cor_lamina_esq">
+            <div className="h1_cor_lamina_esq">
+              <h1>Cor da paleta esquerda</h1>
+            </div>
+            <div className="cor_lamina_esq">
+              <button>Vermelho</button>
+              <button>Preto</button>
+              <button>Azul</button>
+              <button>Verde</button>
+              <button>Branco</button>
+              <button>Amarelo</button>
+            </div>
+          </div>
+
+          {/* Cor da paleta frontal */}
+          <div className="container_cor_lamina_front">
+            <div className="h1_cor_lamina_front">
+              <h1>Cor da paleta frontal</h1>
+            </div>
+            <div className="cor_lamina_front">
+              <button>Vermelho</button>
+              <button>Preto</button>
+              <button>Azul</button>
+              <button>Verde</button>
+              <button>Branco</button>
+              <button>Amarelo</button>
+            </div>
+          </div>
+          
+ {/* Cor da paleta dir */}
+          <div className="container_cor_lamina_dir">
+            <div className="h1_cor_lamina_dir">
+              <h1>Cor da paleta direita</h1>
+            </div>
+            <div className="cor_lamina_dir">
+              <button>Vermelho</button>
+              <button>Preto</button>
+              <button>Azul</button>
+              <button>Verde</button>
+              <button>Branco</button>
+              <button>Amarelo</button>
+            </div>
+          </div>
 
 
 
-<div className='conatiner_m3D'>
-
-<img src="https://i1.sndcdn.com/artworks-7n7INsM9YyE3b2KH-kyEyFw-t1080x1080.jpg" alt=""  className='no_biches_img'/>
 
 
 
 
-</div>
 
-<div className='container_personalizacao_caixa'>
-<div className='container_h1_caixa_personalizada_etp1'>
-<h1 className='h1_caixa_personalizada_etp1'>Personalização da caixa: 1°andar</h1>
-</div>
 
-<div className='container_infos_andares_caixa'>
+        </div>
     
-    <div className='container_andares_caixa'><h2>Andares</h2></div>
-    <div className='container_numero_andares'><h2>1°andar</h2><h2>2°andar</h2><h2>3°andar</h2>    </div>
+      </div>
+    
     
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
-
-   </div>
-
-
-
-
-
-
-
-
-        
-        </div>
-  )
+  );
 }
 
-export default Caixa_persona_etp1
+export default Caixa_persona_etp1;
