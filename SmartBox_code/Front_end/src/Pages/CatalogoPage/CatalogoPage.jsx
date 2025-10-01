b1ce71ec0bb4140acb731e87b85586a5030d5e55
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2025-10-01 17:11:36 -0300

    nada de mais

--- SmartBox_code/Front_end/src/Pages/CatalogoPage/CatalogoPage.jsx
+++ SmartBox_code/Front_end/src/Pages/CatalogoPage/CatalogoPage.jsx
@@ -1,18 +1,70 @@
-import React from 'react'
-import NavBar from '../../Components/NavBar/NavBar.jsx'
-import './CatalogoPage.css'
+import NavBar from "../../Components/NavBar/NavBar.jsx";
+import "./CatalogoPage.css";
 
 function CatalogoPage() {
+  
+  let caixa =[{
+nome:"SmartBox Aventura",
+img:"https://via.placeholder.com/150",
+descricao:"Para os amantes de adrenalina e emoções fortes.",
+preco:"R$ 299,00"
+
+
+  },]
+  
+  
   return (
     <div>
-        
-      <NavBar/>
-        
-        
-        
-        
+      <NavBar />
+      <div className="conteiner_page_catagalogo_box">
+        <div className="container_h1_catalogo_escolha_box">
+          <h1>
+            Escolha sua <span>Smart</span>Box
+          </h1>
         </div>
-  )
+        <div className="container_search_bar">
+          <input
+            type="text"
+            placeholder="O que você procura ?"
+            className="search_input"
+          />
+
+          {/* <button className="search_button">Buscar</button> */}
+        </div>
+<div className="container_h1_boxs">
+
+<h1>Catalogo</h1>
+
+</div>
+
+<div className="container_caixas">
+
+caixa.map((item)=>{
+
+
+
+
+  
+})
+
+
+
+
+
+
+   
+
+
+
+
+
+</div>
+
+
+
+      </div>
+    </div>
+  );
 }
 
-export default CatalogoPage
+export default CatalogoPage;
