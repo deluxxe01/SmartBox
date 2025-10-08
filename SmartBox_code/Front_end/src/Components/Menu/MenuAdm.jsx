d9c5052504e3b1d6f5c061d548e51644ada8724b
deluxxe01 <dudaklincarvalho@gmail.com>
2025-10-08 17:16:58 -0300

    CSS Gestao de caixas

--- SmartBox_code/Front_end/src/Components/Menu/MenuAdm.jsx
+++ SmartBox_code/Front_end/src/Components/Menu/MenuAdm.jsx
@@ -1,19 +1,19 @@
-// import React from 'react'
+import React from 'react'
 import './MenuAdm.css'
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 function MenuAdm() {
-//   const navigate = useNavigate()
+   const navigate = useNavigate()
 
-//   const irParaOutraPagina = () => {
-//     navigate('/')
-// }
   return (
     <div className='MenuAdm-Container'>
      
      <div className ="Nomes">
-        <p>oi</p>
-        <p>oi</p>
-        <p>oi</p>
+        <p className='Fonts1'  onClick={() => navigate('/perfil')}>Conta</p>
+        <p className='Fonts2'  onClick={() => navigate('/Caixa_persona_etp1')}>Dashboard</p>
+        <p className='Fonts3'  onClick={() => navigate('/gestaoCaixas')}>Gestão de Caixas</p>
+        <p className='Fonts1'  onClick={() => navigate('/Caixa_persona_etp1')}>Gestão de Pedidos</p>
+        <p className='Fonts2'  onClick={() => navigate('/Caixa_persona_etp1')}>Gestão de Produtos</p>
+        <p className='Fonts3'  onClick={() => navigate('/Caixa_persona_etp1')}>Gestão de Clientes</p>
      </div>
     </div>
   )
--- SmartBox_code/Front_end/src/Pages/GestaoCaixasPages/GestaoCaixas.jsx
+++ SmartBox_code/Front_end/src/Pages/GestaoCaixasPages/GestaoCaixas.jsx
@@ -6,12 +6,27 @@ function GestaoCaixas() {
   return (
     <div className='Container-GestaoCaixas'>
         
+      <h1 className='Titulo-Dashboard'>Gestão de Caixas</h1>
+      <MenuAdm />
+<div className='ContainerInfosCaixa'>
+     <input className="InputSearch" type="text" />
+     <img  className='LixeiraGestao' src="public/icon/deleteblack.png"alt="Botão para excluir conta"/>
+
+     <div className='ContainerInfosCaixa2'>
+      <p className=''>+ Adcionar uma imagem da caixa</p>
+       <div className='InptDescrição'>
+        <label htmlFor="">Descrição</label>
+        <input  className="InptDes"type="text" />
+       </div>
+     </div>
+
+     <p className='P-VisuCaixa'>Visualização da Caixa</p>
+     <div className='Container-VisuCaixa'>
       
-        <MenuAdm />
+     </div> 
+</div> 
         
-        
-        
-        </div>
+      </div>
   )
 }
 
