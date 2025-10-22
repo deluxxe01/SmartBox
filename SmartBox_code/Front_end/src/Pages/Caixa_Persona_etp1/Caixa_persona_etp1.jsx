2a7cfafb5f2a8469c4f7f4ee9d4134d03ab2da17
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2025-10-22 17:04:59 -0300

    refatorando a  pagina de personalizar a caixa

--- SmartBox_code/Front_end/src/Pages/Caixa_Persona_etp1/Caixa_persona_etp1.jsx
+++ SmartBox_code/Front_end/src/Pages/Caixa_Persona_etp1/Caixa_persona_etp1.jsx
@@ -1,371 +1,153 @@
-import "./Caixa_persona_etp1.css";
-import React, { useState, useContext } from "react";
-import axios from "axios";
-import NavBar from "../../Components/NavBar/NavBar";
-import { useNavigate } from "react-router-dom";
-import { GlobalContext } from "../../Context/Globalcontext";
-
-function SeletorCor({ titulo, corSelecionada, setCor }) {
-  const cores = ["1", "preto", "azul", "verde", "branco", "amarelo"];
- 
-
-  return (
-    <div>
-      <div className="titulo_secao">
-        <p>{titulo}</p>
-      </div>
-      <div className="cores_container">
-        {cores.map((cor) => (
-          <div
-            key={cor}
-            className={`container_cor_bnt ${
-              corSelecionada === cor ? "selecionado" : ""
-            }`}
-          >
-            <button
-              className={`cor_bnt ${cor}`}
-              onClick={() => setCor(cor)}
-            ></button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function SeletorDesenho({ titulo, desenhoSelecionado, setDesenho }) {
-  const desenhos = [
-    { nome: "Barco", img: "./icon/barco_desenho.svg", cor: "#CB0019" },
-    { nome: "Estrela", img: "./icon/estrela_desenho.svg", cor: "#2A7DF0" },
-    { nome: "Casa", img: "./icon/casa_desenho.svg", cor: "#3DAE00" },
-    { nome: "Sem desenho", img: "./icon/sem_image.svg", cor: "#222222" },
-  ];
-
-  return (
-    <div>
-      <div className="titulo_secao">
-        <p>{titulo}</p>
-      </div>
-      <div className="desenhos_container">
-        {desenhos.map((d) => (
-          <button
-            key={d.nome}
-            style={{ backgroundColor: d.cor }}
-            className={`desenho_bnt ${
-              desenhoSelecionado === d.nome ? "selecionado" : ""
-            }`}
-            onClick={() => setDesenho(d.nome)}
-          >
-            <img src={d.img} alt={d.nome} className="desenho_img" />
-          </button>
-        ))}
-      </div>
-    </div>
-  );
-}
+import React from 'react'
+import NavBar from '../../Components/NavBar/NavBar'
+import './Caixa_persona_etp1.css'
 
 function Caixa_persona_etp1() {
-  const [etapa, setEtapa] = useState("cores");
-  const [andares, setAndares] = useState(1);
-  const [maxAndarLiberado, setMaxAndarLiberado] = useState(1);
-  const [erroMsg, setErroMsg] = useState("");
-  const { usuarioAtual } = useContext(GlobalContext);
-  const navigate = useNavigate();
-
-  const [personalizacoes, setPersonalizacoes] = useState({
-    1: {
-      corChassi: null,
-      corLaminaEsq: null,
-      corLaminaFront: null,
-      corLaminaDir: null,
-      desenhoLaminaEsq: null,
-      desenhoLaminaFront: null,
-      desenhoLaminaDir: null,
-    },
-    2: {
-      corChassi: null,
-      corLaminaEsq: null,
-      corLaminaFront: null,
-      corLaminaDir: null,
-      desenhoLaminaEsq: null,
-      desenhoLaminaFront: null,
-      desenhoLaminaDir: null,
-    },
-    3: {
-      corChassi: null,
-      corLaminaEsq: null,
-      corLaminaFront: null,
-      corLaminaDir: null,
-      desenhoLaminaEsq: null,
-      desenhoLaminaFront: null,
-      desenhoLaminaDir: null,
-    },
-  });
-
-  // Atualiza cor de uma parte do andar
-  const atualizarCor = (campo, valor) => {
-    setPersonalizacoes((prev) => ({
-      ...prev,
-      [andares]: { ...prev[andares], [campo]: valor },
-    }));
-    setErroMsg("");
-  };
-
-  // Atualiza desenho de uma lâmina
-  const atualizarDesenho = (lamina, valor) => {
-    setPersonalizacoes((prev) => ({
-      ...prev,
-      [andares]: { ...prev[andares], [lamina]: valor },
-    }));
-    setErroMsg("");
-  };
-
-  const todasCoresSelecionadas = (andar) => {
-    const config = personalizacoes[andar];
-    return (
-      config.corChassi &&
-      config.corLaminaEsq &&
-      config.corLaminaFront &&
-      config.corLaminaDir
-    );
-  };
-
-  const todosDesenhosSelecionados = (andar) => {
-    const config = personalizacoes[andar];
-    return (
-      config.desenhoLaminaEsq &&
-      config.desenhoLaminaFront &&
-      config.desenhoLaminaDir
-    );
-  };
-
-  const trocarAndar = (novoAndar) => {
-    if (novoAndar >= 1 && novoAndar <= 3) {
-      setAndares(novoAndar);
-      setMaxAndarLiberado((prev) => Math.max(prev, novoAndar));
-      setErroMsg("");
-    }
-  };
-
-  
-
-    const enviarCaixa = async () => {
-      await axios.post('api/createbox',{
-       
-    payload:{
-
-      caixa:{
-        
-        bloco1:{
-          bloco1_cor_chasi:personalizacoes[1].corChassi,
-         bloco1_lamina_cor_esq:personalizacoes[1].corLaminaEsq,
-         bloco1_lamina_cor_front:personalizacoes[1].corLaminaFront,
-        bloco1_lamina_cor_dir:personalizacoes[1].corLaminaDir,
-        bloco1_desenho_lamina_dir:personalizacoes[1].desenhoLaminaEsq,
-        bloco1_desenho_lamina_front:personalizacoes[1].desenhoLaminaFront,
-        bloco1_desenho_lamina_esq:personalizacoes[1]. desenhoLaminaDir,
-      },
-        bloco2:{
-        bloco2_cor_chasi:personalizacoes[2].corChassi,
-        bloco2_lamina_cor_esq:personalizacoes[2].corLaminaEsq,
-        bloco2_lamina_cor_front:personalizacoes[2].corLaminaFront,
-        bloco2_lamina_cor_dir:personalizacoes[2].corLaminaDir,
-        bloco2_desenho_lamina_dir:personalizacoes[2].desenhoLaminaEsq,
-        bloco2_desenho_lamina_front:personalizacoes[2].desenhoLaminaFront,
-        bloco2_desenho_lamina_esq:personalizacoes[2].desenhoLaminaDir,
-        
-        },
-        bloco3:{
-          bloco3_cor_chasi:personalizacoes[3].corChassi,
-        bloco3_lamina_cor_esq:personalizacoes[3].corLaminaEsq,
-        bloco3_lamina_cor_front:personalizacoes[3].corLaminaFront,
-        bloco3_lamina_cor_dir:personalizacoes[3].corLaminaDir,
-        bloco3_desenho_lamina_dir:personalizacoes[3].desenhoLaminaEsq,
-        bloco3_desenho_lamina_front:personalizacoes[3].desenhoLaminaFront,
-        bloco3_desenho_lamina_esq:personalizacoes[3].desenhoLaminaDir,
-        
-      },
-    
-    },
-    sku:'caixinha',
-          
-  } ,
-   
-   
-    callbackUrl: "http://localhost:3333/callback"
-  }
-  
-  )
-   
-const infosCaixa = async ()=>{
-
-await axios.post('api/caixa',{
-
-fk_id_cliente:usuarioAtual,
-nome:'1',
-
-
-
-
-})
-
-
-}
-
-
-
-
-
-
-
-
-}
-  const atual = personalizacoes[andares];
-
-console.log(personalizacoes)
-
-
   return (
-    <div className="container_page_caixa_persona">
-      <div className="container_m3D">
-        <div className="container_select_de_paginas">
-          <img
-            src="./images/logo_smartBox.svg"
-            className="img_logo_select_paginas"
-            alt="Logo"
-          />
-        </div>
-      </div>
+   <div className='container_caixa_persona_etp1'>
 
-      <div className="container_personalizacao_caixa">
-        <div>
-          <p className="p_caixa_personalizada_etp1">
-            Personalização ({etapa === "cores" ? "Cores" : "Desenhos"}): {andares}° andar
-          </p>
+<div className='conatiner_head_caixa_persona'>
+   <NavBar/>
+</div>
 
-          {/* Seletor de andares */}
-          <div>
-            <div className="titulo_secao">
-              <p>Andares</p>
-            </div>
-            <div className="container_numero_andares">
-              {[1, 2, 3].map((num) => {
-                const validacaoParaAvancar =
-                  etapa === "cores"
-                    ? todasCoresSelecionadas(andares)
-                    : todosDesenhosSelecionados(andares);
-                const podeVoltar = num < andares;
-                const podeAvancar = num === andares + 1 && validacaoParaAvancar;
-                const mesmoAndar = num === andares;
+<div className='container_custom_caixa_e_mD3'>
 
-                const habilitado = podeVoltar || podeAvancar || mesmoAndar;
+<div className='container_modelo_3D'>
 
-                return (
-                  <button
-                    key={num}
-                    className={`bnt_andar ${
-                      andares === num ? "andar_selecionado" : ""
-                    } ${!habilitado ? "andar_bloqueado" : ""}`}
-                    onClick={() => habilitado && trocarAndar(num)}
-                    disabled={!habilitado}
-                  >
-                    {num}° andar
-                  </button>
-                );
-              })}
-            </div>
-          </div>
+<img src="https://pbs.twimg.com/media/FDtRc36VEAIhgER.png" alt="" className='modelo_3D'/>
 
-          {/* Escolha de cores ou desenhos */}
-          {etapa === "cores" ? (
-            <>
-              <SeletorCor
-                titulo="Cor do chassi"
-                corSelecionada={atual.corChassi}
-                setCor={(c) => atualizarCor("corChassi", c)}
-              />
-              <SeletorCor
-                titulo="Cor da paleta esquerda"
-                corSelecionada={atual.corLaminaEsq}
-                setCor={(c) => atualizarCor("corLaminaEsq", c)}
-              />
-              <SeletorCor
-                titulo="Cor da paleta frontal"
-                corSelecionada={atual.corLaminaFront}
-                setCor={(c) => atualizarCor("corLaminaFront", c)}
-              />
-              <SeletorCor
-                titulo="Cor da paleta direita"
-                corSelecionada={atual.corLaminaDir}
-                setCor={(c) => atualizarCor("corLaminaDir", c)}
-              />
-            </>
-          ) : (
-            <>
-              <SeletorDesenho
-                titulo="Lâmina Esquerda"
-                desenhoSelecionado={atual.desenhoLaminaEsq}
-                setDesenho={(d) => atualizarDesenho("desenhoLaminaEsq", d)}
-              />
-              <SeletorDesenho
-                titulo="Lâmina Frontal"
-                desenhoSelecionado={atual.desenhoLaminaFront}
-                setDesenho={(d) => atualizarDesenho("desenhoLaminaFront", d)}
-              />
-              <SeletorDesenho
-                titulo="Lâmina Direita"
-                desenhoSelecionado={atual.desenhoLaminaDir}
-                setDesenho={(d) => atualizarDesenho("desenhoLaminaDir", d)}
-              />
-            </>
-          )}
+</div>
 
-          {erroMsg && <p className="mensagem_erro">{erroMsg}</p>}
-        </div>
+<div className='container_custom_caixas'>
 
-        {/* Botões de navegação */}
-        <div className="container_bnt_proxima_etapa">
-          {etapa === "desenhos" && (
-            <button
-              className="bnt_voltar_etapa"
-              onClick={() => {
-                setEtapa("cores");
-                setErroMsg("");
-              }}
-            >
-              Voltar
-            </button>
-          )}
 
-          <button
-            className="bnt_proxima_etapa"
-            onClick={() => {
-              if (etapa === "cores") {
-                for (let i = 1; i <= maxAndarLiberado; i++) {
-                  if (!todasCoresSelecionadas(i)) {
-                    setErroMsg(
-                      `Por favor, complete a seleção de cores do ${i}° andar antes de continuar.`
-                    );
-                    return;
-                  }
-                }
-                setErroMsg("");
-                setEtapa("desenhos");
-                setAndares(1); // começa os desenhos no 1º andar, mas pode trocar
-              } else {
-                
-              }
-            }}
-          >
-            {etapa === "cores" ? "Próxima etapa" : "Finalizar"}
-          </button>
 
-        <button onClick={()=>{enviarCaixa()}}>enviar caixa</button>
-       
-        </div>
-      </div>
-    </div>
-  );
+
+
+
+
+<div className='title_custom_caixa'>
+<h1>Personalização da Caixa 1</h1>
+</div>
+
+<div className='container_all_andares_caixa'>
+
+
+
+<div className='container_andares_custom_caixa'>
+<h1>andares</h1>
+</div>
+<div className='container_andares_caixa'>
+
+<button >1 andar</button>
+<button>2 andar</button>
+<button>3 andar</button>
+
+</div>
+</div>
+
+<div className='container_cor_chasi'>
+
+<button className='button_cor_chasi_vermelho'></button>
+<button className='button_cor_chasi_preto'></button>
+<button className='button_cor_chasi_azul'></button>
+</div>
+
+
+
+
+
+<div className='cotainer_laminas_esq_cores'>
+
+<button>sem lamina</button>
+<button>vermelho</button>
+<button>preto</button>
+<button>azul</button>
+<button>verde</button>
+<button>branco</button>
+<button>amarelo</button> 
+
+</div>
+
+
+
+<div className='cotainer_laminas_front_cores'>
+
+<button>sem lamina</button>
+<button>vermelho</button>
+<button>preto</button>
+<button>azul</button>
+<button>verde</button>
+<button>branco</button>
+<button>amarelo</button> 
+
+</div>
+
+
+
+
+<div className='cotainer_laminas_dir_cores'>
+
+<button>sem lamina</button>
+<button>vermelho</button>
+<button>preto</button>
+<button>azul</button>
+<button>verde</button>
+<button>branco</button>
+<button>amarelo</button> 
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+   </div>
+  
+
+  )
 }
 
-export default Caixa_persona_etp1;
+export default Caixa_persona_etp1
